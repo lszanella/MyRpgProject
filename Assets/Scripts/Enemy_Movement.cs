67fc180cf4e52a2a428cbe380c9ca6398094875a
Lucas S. Zanella <luczasp@me.com>
2024-11-06 16:05:44 -0300

    First test GH pages

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -1,6 +1,5 @@
 using System;
 using Unity.VisualScripting;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.XR;
 
--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -1,4 +1,3 @@
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
